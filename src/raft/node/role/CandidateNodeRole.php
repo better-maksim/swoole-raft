f7a831b0f2fe6b3f3b92df0a864b93a76893ec6a
Better-Maksim <guozhu123@gmail.com>
2020-10-20 00:07:12 +0800

    增加角色模型

---
+++ src/raft/node/role/CandidateNodeRole.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace raft\node\role;
+
+class CandidateNodeRole extends AbstractNodeRole
+{
+
+}
--- src/raft/node/role/FollowerNodeRole.php
+++ src/raft/node/role/FollowerNodeRole.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace raft\node\role;
 
 use raft\node\NodeId;
